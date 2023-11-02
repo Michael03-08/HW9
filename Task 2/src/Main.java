07b7af4d2c0fd1b27886ae33758587d5d7361687
Flex <cma491628@gmail.com>
2023-11-02 19:03:57 +0200

    Работа с методами урок 2

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+
+        truthCheck();
+    }
+
+    public static void truthCheck(){
+        System.out.print("Введите число: ");
+        int num = scanner.nextInt();
+        if (num % 2 == 0){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
     }
 }
