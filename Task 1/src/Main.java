2e20a6299fbcbac6c8fc05f3bc9a0e1e35a37f6a
Flex <cma491628@gmail.com>
2023-11-02 17:45:42 +0200

    Работа с методом

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
+    static Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
         //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор
@@ -15,22 +19,36 @@ public class Main {
 
     private static void divide() {
         //todo дописать логику и печать результата
+       double result = a / b;
+       if (b == 0){
+           System.out.println("На ноль делить нельзя!");
+       }else {
+           System.out.println("Результат деления: " + result);
+       }
     }
 
     private static void diff() {
         //todo дописать логику и печать результата
+        double result = a - b;
+        System.out.println("Результат вычитания: " + result);
     }
 
     private static void multiply() {
         //todo дописать логику и печать результата
+        double result = a * b;
+        System.out.println("Результат умножения: " + result);
     }
 
     private static void sum() {
         //todo дописать логику и печать результата
+        double result = a + b;
+        System.out.println("Результат сложения: " + result);
     }
 
     private static double inputDouble() {
         //todo дописать логику
-        return 0;
+        System.out.print("Введите число: ");
+        double inputNum = scanner.nextDouble();
+        return inputNum;
     }
 }
